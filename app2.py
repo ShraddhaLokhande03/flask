7c5df95c12524b20285fbc44ae91a6f9f7974f5b
Shraddha Lokhande <95165557+ShraddhaLokhande03@users.noreply.github.com>
2024-07-07 19:31:58 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 # @app.route('/')
 # def Welcome():
-#     return "good morning"
+#     return "good morning"----simple start
 
 @app.route('/Pass/<int:score>')
 def Pass(score):

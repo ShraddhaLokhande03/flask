84be7a34621d9de9b422e1b7a93e4105646bc1a3
Shraddha <lshraddha2002@gmail.com>
2024-07-07 20:54:23 +0530

    thirdcommit

--- app3.py
+++ app3.py
@@ -1,13 +1,41 @@
 # 1. Integrate Html with flask
 # 2. HTTP Varibles GET & POST
 
-from flask import Flask,redirect,url_for,render_template
+from flask import Flask,redirect,url_for,render_template ,request
 
 app = Flask(__name__)
 
 @app.route('/')
 def Welcome():
     return render_template("index.html")
+
+@app.route('/Pass/<int:score>')
+def Pass(score):
+    res=""
+    if score>=50:
+        res="PASS"
+    else:
+        res="FAIL"
+    return render_template('result.html',result=res)
+
+@app.route('/Fail/<int:score>')
+def Fail(score):
+    return "The Person has failed with "+ str(score)
+
+@app.route('/submit',methods = ['POST',"GET"])
+def submit():
+    total_Score=""
+
+    if request.method=='POST':
+        science = float(request.form['science'])
+        maths = float(request.form['maths'])
+        c = float(request.form['c'])
+        data_science = float(request.form['datascience'])
+        total_Score=(science+maths+c+data_science)/4
+    
+    return redirect(url_for("Pass",score=total_Score))
+
+
 
 if __name__ =="__main__":
     app.run(debug=True)
